7e7510a3452ad9228ea6d68103b783d0a7e08c8e
hcuevasbc <hcuevas@brightcove.com>
2022-03-29 13:11:27 -0600

    First commit

---
+++ Simulive/Simulive.h
@@ -0,0 +1,18 @@
+//
+//  Simulive.h
+//  Simulive
+//
+//  Created by Hector Cuevas on 29/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Simulive.
+FOUNDATION_EXPORT double SimuliveVersionNumber;
+
+//! Project version string for Simulive.
+FOUNDATION_EXPORT const unsigned char SimuliveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simulive/PublicHeader.h>
+
+
